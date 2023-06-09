7a7db18912633ac550240a09b6e1a4612751008c
Sasikant Biswal <biswalsashikant@gmail.com>
2023-06-09 20:50:00 +0530

    Added some svg fonts and icons

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav id="header" className="bg-black text-white">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+            <div className="logo-wraapper pl-4 flex items-center">
+                <img src={"/"} alt="logo" />
+            </div>
+            <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
+                <div>Home</div>
+                <div>About</div>
+            </div>
+            <div className="flex items-center justify-center space-x-4">
+                <div>Cart</div>
+                <div>Log In</div>
+                <div>Sign Up</div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Header;
